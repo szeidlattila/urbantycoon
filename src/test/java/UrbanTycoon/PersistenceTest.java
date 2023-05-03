d2af479631479e6665b9e2e783b622601eefc650
Sándor Kornél <z4prn0@inf.elte.hu>
2023-05-03 23:10:01 +0200

    fix3. kiderült hogy nem példányosíthat JFrame-t gitlab miatt

--- src/test/java/UrbanTycoon/PersistenceTest.java
+++ src/test/java/UrbanTycoon/PersistenceTest.java
@@ -19,17 +19,47 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 public class PersistenceTest {
+    private final int WIDTH = 80;
+    private final int HEIGHT = 80;
+    private final int FIELDSIZE = 20;
+    private final int FIELDROWSNUM = 8;
+    private final int FIELDCOLSNUM = 16;
+    private final int INITIALMONEY = 100000;
+    private final int INITIALRESIDENT = 20;
+    private final int ZONEPRICE = 250;
+    private final int ROADPRICE = 75;
+    private final int STADIUMPRICE = 1000;
+    private final int POLICESTATIONPRICE = 750;
+    private final int FIRESTATIONPRICE = 750;
+    private final int FORESTPRICE = 500;
+    private final double ANNUALFEEPERCENTAGE = 0.3; // playerBuildIt annualFee = price * ANNUALFEEPERCENTAGE
+    private final int RESIDENTCAPACITY = 5;
+    private final int WORKPLACECAPACITY = 18;
+    private final double REFUND = 0.4;
+    private final int RADIUS = 3;
+    private final int CRITSATISFACTION = -5;
+    private final int MOVEINATLEASTSATISFACTION = 5;
+    private final double CHANCEOFFIRE = 0.05;
     
     static City city;
+    static City city2;
     
     @BeforeEach
     public void setUp(){
-        city = new GameEngine().getCity();
+        city = new City(INITIALRESIDENT, FIELDSIZE, FIELDROWSNUM, FIELDCOLSNUM, CRITSATISFACTION,
+                MOVEINATLEASTSATISFACTION, INITIALMONEY,
+                ZONEPRICE, ROADPRICE, STADIUMPRICE, POLICESTATIONPRICE, FIRESTATIONPRICE, FORESTPRICE,
+                ANNUALFEEPERCENTAGE,
+                RESIDENTCAPACITY, WORKPLACECAPACITY, REFUND, CHANCEOFFIRE, RADIUS, WIDTH, HEIGHT);
+        city2 = new City(INITIALRESIDENT, FIELDSIZE, FIELDROWSNUM, FIELDCOLSNUM, CRITSATISFACTION,
+                MOVEINATLEASTSATISFACTION, INITIALMONEY,
+                ZONEPRICE, ROADPRICE, STADIUMPRICE, POLICESTATIONPRICE, FIRESTATIONPRICE, FORESTPRICE,
+                ANNUALFEEPERCENTAGE,
+                RESIDENTCAPACITY, WORKPLACECAPACITY, REFUND, CHANCEOFFIRE, RADIUS, WIDTH, HEIGHT);
     }
     
     @Test
     public void loadSameGame(){
-        City city2 = new GameEngine().getCity();
         for(int i=0;i<city.getFields().length;i++)
             for(int j=0; j<city.getFields()[0].length;j++)
                 assertEquals(city.getFields()[i][j],city2.getFields()[i][j]);
@@ -42,7 +72,6 @@ public class PersistenceTest {
     }
     @Test
     public void changeThenLoad(){
-        City city2 = new GameEngine().getCity();
         city.fieldSelect( 5,1);
         city.build(Stadium.class);
         city.fieldSelect(6,0);
